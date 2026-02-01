e2945ded86c841f969cb3635b950ba3f8802f429
Mohammad Mahdi Khaligh <132351751+gatemans@users.noreply.github.com>
2026-02-01 21:05:19 +0330

    Update data_plot.m

--- data_plot.m
+++ data_plot.m
@@ -1,1 +1,13 @@
 
+data = readtable('data.csv');
+
+x_acc = table2array(data(:,5));
+y_acc = table2array(data(:,6));
+z_acc = table2array(data(:,7));
+
+n = length(x_acc);
+num_sampling = 1:n;
+
+plot(num_sampling,z_acc);
+plot(num_sampling,y_acc);
+plot(num_sampling,x_acc);
